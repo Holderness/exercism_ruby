a07df0d958a50875a0e4f819aa2933481bb2c460
Holderness <andersonholderness@gmail.com>
2015-02-09 16:01:02 -0500

    refactors, takes out temporary variable

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 class Raindrops
 
@@ -7,13 +6,13 @@ class Raindrops
     5 => "Plang",
     7 => "Plong"
   }
-  
+
   def self.convert(num)
-    drop_str = ""
-  	DROPS.keys.each do |i|
-      drop_str += DROPS[i] if num % i == 0
-    end
+    drop_str = DROPS.each_key.map do |i|
+      DROPS[i] if num % i == 0
+    end.join
     drop_str.empty? ? num.to_s : drop_str
   end
+
 
 end
