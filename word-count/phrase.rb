e1f48f1a26fdaa6ffad7a7f859299098bf6f84f1
Holderness <andersonholderness@gmail.com>
2015-03-07 23:12:32 -0500

    one more brevity refactor

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -1,20 +1,16 @@
 class Phrase
 
-  attr_accessor :text, :word_count
+  attr_accessor :words, :word_count
 
   def initialize(text)
-    @text = text
+    @words = text.downcase.split(/[^a-zA-Z0-9']+/)
     @word_count ||= creates_word_hash
   end
 
   def creates_word_hash
-    word_array.each_with_object(Hash.new(0)) do |word, hash|
+    words.each_with_object(Hash.new(0)) do |word, hash|
       hash[word] += 1
     end
-  end
-
-  def word_array
-    text.downcase.split(/[^a-zA-Z0-9']+/)
   end
 
 end
