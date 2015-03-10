90491c3d12bd604cf210eb551a7118a101a31e7b
Holderness <andersonholderness@gmail.com>
2015-03-10 09:55:38 -0400

    refactors robot-name, adds private methods

--- robot-name/robot.rb
+++ robot-name/robot.rb
@@ -1,10 +1,14 @@
 class Robot
 
-  attr_accessor :name
-
-  def initialize
-    @name ||= creates_name
+  def name
+    @name ||= create_name
   end
+
+  def reset
+    @name = create_name
+  end
+
+  private
 
   def random_letters
     ('A'..'Z').to_a.shuffle[0,2].join
@@ -14,12 +18,8 @@ class Robot
     rand(100..999).to_s
   end
 
-  def creates_name
+  def create_name
     random_letters + random_numbers
-  end
-
-  def reset
-    @name = creates_name
   end
 
 end
