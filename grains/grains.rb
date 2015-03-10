c377e03b544d917d9998932a5a889fb8dcc29813
Holderness <andersonholderness@gmail.com>
2015-03-09 23:17:09 -0400

    refactors grains to avoid repeated calculations

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,10 +1,17 @@
+
 class Grains
+
+	attr_reader :total
+
+	def initialize
+    @total ||= calculate_total
+	end
   
   def square(num)
-    (1..num).inject { |sum| sum * 2 } 
+    (1..num).inject { |sum| sum * 2 }
   end
 
-  def total
+  def calculate_total
     square(65) - 1
   end
 
